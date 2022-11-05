f7c2063ac6bef20f37fd93d1bed025da87ca265a
puhu17@gmail.com <puhu17@gmail.com>
2022-11-05 22:56:13 +0900

    view

---
+++ addressapiview/src/main/webapp/popup.js
@@ -0,0 +1,5 @@
+	const showpopup= ()=>{
+		window.name="parent";
+		window.open("http://puhu17.cafe24.com/addressapi/getdata.html","주소검색 팝업","width=350px,height=800px");
+	}
+	res.setHeader('Access-Control-Allow-origin', 'http://puhu17.cafe24.com');
