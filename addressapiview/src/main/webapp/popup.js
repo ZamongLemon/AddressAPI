8233ae53c889ac10a863d99c30ed2b397eec9a18
puhu17@gmail.com <puhu17@gmail.com>
2022-11-06 17:39:13 +0900

    ㅊ

--- addressapiview/src/main/webapp/popup.js
+++ addressapiview/src/main/webapp/popup.js
@@ -1,5 +1,4 @@
 	const showpopup= ()=>{
 		window.name="parent";
-		window.open("http://puhu17.cafe24.com/addressapi/getdata.html","주소검색 팝업","width=350px,height=800px");
+		window.open("./getdata.jsp","주소검색 팝업","width=350px,height=800px");
 	}
-	res.setHeader('Access-Control-Allow-origin', 'http://puhu17.cafe24.com');
